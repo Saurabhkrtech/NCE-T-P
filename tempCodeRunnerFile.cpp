42ba2e7464176cb1adbc6ed591e76d3fa93dc248
Saurabh <saurabhkr00112233@gmail.com>
2024-07-11 01:27:51 +0530

    second commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+    using namespace std;
+    
+    int main(){
+        for(int i=0;i<5;i++){
+        cout<<"hello saurabh"<<endl;
+        }
+        int arr[]={20,30,56,54};
+        for(int i=0;i<4;i++){
+            cout<<arr[i]<<endl;
+        }
+        return 0;
+    }
